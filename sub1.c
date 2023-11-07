326fac399e6230445b82bbcff0f15131890f974d
dlalstmd <minid0329@gmail.com>
2023-11-07 15:40:57 +0900

    test1

---
+++ sub1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void sort(int arr[]){
+    //코드 작성
+
+    for(int i=0 ; i<10 ; i++){
+        printf("%d ", arr[i]);
+    }
+}
+
+int main(){
+    int Array[10] = {3,5,2,1,4,8,10,7,6,9};
+    sort(Array);
+}
